49ce89b54b7b1e2f9dd9c1d060e813c8889b9135
hardwin27 <hardwin27@gmail.com>
2022-02-16 16:12:55 +0800

    Add StoryPopup

---
+++ Assets/Scripts/InterfaceScript/StoryPopUp/ChapterButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChapterButton : MonoBehaviour
+{
+    [SerializeField] private Text buttonText;
+    [SerializeField] private Sprite[] buttonSprites;
+    private Button button;
+    private Image buttonImage;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        buttonImage = GetComponent<Image>();
+    }
+
+    public void SetButton(string buttonString, bool isUnlocked)
+    {
+        buttonText.text = buttonString;
+        button.interactable = isUnlocked;
+        buttonImage.sprite = buttonSprites[System.Convert.ToInt32(isUnlocked)];
+    }
+}
---
+++ Assets/Scripts/InterfaceScript/StoryPopUp/StoryPopUpManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryPopUpManager : MonoBehaviour
+{
+    //Dummy Database
+    private string[] buttonsText = { 
+        "Chapter 1", 
+        "Chapter 2", 
+        "Coming Soon" 
+    };
+    private bool[] areButtonsUnlocked = { 
+        true, 
+        false, 
+        false 
+    };
+
+    [SerializeField] private Transform chapterButtonParent;
+    [SerializeField] private GameObject chapterButton;
+
+    private void Start()
+    {
+        for(int bttnIndex = 0; bttnIndex < buttonsText.Length; bttnIndex++)
+        {
+            GameObject newChButtonObj = Instantiate(chapterButton, chapterButtonParent);
+            ChapterButton newChButton = newChButtonObj.GetComponent<ChapterButton>();
+
+            newChButton.SetButton(buttonsText[bttnIndex], areButtonsUnlocked[bttnIndex]);
+        }
+    }
+}
